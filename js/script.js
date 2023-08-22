d695638d864e2827aeaabe8b7b6850805dbf52c1
yeibin02 <yeibin02@gmail.com>
2023-08-22 20:54:04 +0900

    feat : header 구조 진행
    header의 html 구조 및 css 적용 완료
    header js 작업 진행 예정

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 // dom (html 태그 로딩 완료 후 실행)
-$(document).ready(function () { });
+$(document).ready(function () {
+  // 클래스 nav 의 li 를 찾아라
+  // 저장한다. 재활용하기 위해서
+  const navLis = $(".nav ul li");
+});
 // 멀티미디어 리소스 로딩 완료 후 실행
-window.onload = function(){}
+window.onload = function () {
+  // 클래스 nav 의 li 를 찾아라
+  // document.querySelector(".nav ul li")
+  const navLis = document.querySelectorAll(".nav ul li");
+};
